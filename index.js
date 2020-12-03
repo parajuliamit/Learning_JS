6892e308ae0161b051f90b99fe0f340e91eb0e6c
parajuliamit <be2017se587@gces.edu.np>
2020-12-03 23:39:40 +0545

    day 2

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 document.write('Hello World');
+
+
+// let data = {first_name:'Amit',last_name:'Parajuli'};
+// console.log(data.last_name);
+
+function clicked(){
+    console.log('clicked');
+}
+
+// let arr = ['f','b','z','d'];
+// arr.forEach(function (value,index){
+// console.log(value);
+// });
+
+// let sliceArr = arr.slice(1,3);
+// console.log(sliceArr);
+// console.log(arr.sort());
+// console.log(arr.reverse());
+
+// let fruits = ['Apple','Orange','Mango'];
+// delete fruits[0];
+// fruits.splice(0,1,"Kiwi");
+// let veggies = ['Cauli','Potato'];
+// let all = fruits.concat(veggies);
+// console.log(all);
+// console.log(fruits);
+// console.log(fruits.length);
+// console.log(fruits.pop());
+
+// function book(title,author){
+//     this.title = title;
+//     this.author = author;
+// }
+
+// let myBook = new book("Amit", "Book");
+// console.log(myBook.author);
+// console.log(myBook); 
